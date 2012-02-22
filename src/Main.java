8e82fdfe8695830fa849a41bf75bc9331eff4a90
Michael Zhou <zhoumotongxue008@gmail.com>
2012-02-22 07:07:55 -0500

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import model.*;
 import view.*;
+
 import javax.swing.JFrame;
 
 public class Main {
@@ -10,12 +11,12 @@ public class Main {
     public static void main(String[] args) {
 	TivooModel model = new TivooModel();
     	// create program specific components
-        TivooViewer display = new TivooViewer(model);
+        //TivooViewer display = new TivooViewer(model);
         // create container that will work with Window manager
         //JFrame frame = new JFrame(TITLE);
         // add our user interface components to Frame and show it
         // start somewhere, less typing for debugging
-        display.showPage(DEFAULT_START_PAGE);
+        //display.showPage(DEFAULT_START_PAGE);
 	
 	String input = "dukecal.xml", outputsummary = "output/testhtml.html", 
 		outputdetails = "output/details/";
@@ -26,7 +27,5 @@ public class Main {
 	}
 	
     }
-
-    
     
 }
